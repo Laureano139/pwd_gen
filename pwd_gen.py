dbfc28638c1e64a914b84312e1834a88604da325
Hendrix <afamorim16@sapo.pt>
2025-08-17 20:20:33 +0100

    New functionalities
    TODO -> Encryptation

--- pwd_gen.py
+++ pwd_gen.py
@@ -1,8 +1,28 @@
 import os, random, string, json
 
-#TODO -> Encrypt json file and passwords | App menu | Option to get a password from a platform account
+#TODO -> Encrypt json file and passwords
 
 def main():
+    print("Welcome to the Password Generator!\n")
+    print("1 - Generate a new password")
+    print("2 - Retrieve an existing password")
+    print("3 - Change email/password")
+    print("4 - Exit")
+
+    choice = input("Enter your choice (1/2/3/4): ").strip()
+    if choice == "1":
+        add_new_acc()
+    elif choice == "2":
+        retrieve_password()
+    elif choice == "3":
+        change_email_password()
+    elif choice == "4":
+        exit()
+    else:
+        print("Invalid choice. Please try again.")
+        main()
+
+def add_new_acc():
     platform = input("Enter the platform name: ").strip()
     with open("./accounts/accounts.json", "r") as f:
         if os.path.getsize("./accounts/accounts.json") == 0:
@@ -14,8 +34,8 @@ def main():
             continue
         elif i["Platform"].lower() == platform.lower():
             print("Existing account found! Choose one option: \n")
-            print("1 - Show existing password")
-            print("2 - Generate new password")
+            print("1 - Show the existing password")
+            print("2 - Generate a new password and update")
             print("3 - Cancel")
             choice = input("Enter your choice (1/2/3): ").strip()
             if choice == "1":
@@ -56,26 +76,6 @@ def main():
 
         with open("./accounts/accounts.json", "w") as f:
             json.dump(accounts, f, indent=4)
-            
-def main_menu():
-    print("Welcome to the Password Generator!\n")
-    print("1 - Generate a new password")
-    print("2 - Retrieve an existing password")
-    print("3 - Change email/password")
-    print("4 - Exit")
-
-    choice = input("Enter your choice (1/2/3/4): ").strip()
-    if choice == "1":
-        main()
-    # elif choice == "2":
-    #     retrieve_password()
-    # elif choice == "3":
-    #     change_email_password()
-    # elif choice == "4":
-    #     exit()
-    else:
-        print("Invalid choice. Please try again.")
-        main_menu()
 
 def gen_pwd():
     pwd_length = 12
@@ -101,6 +101,75 @@ def gen_pwd():
     print("Generated Password: ", mixed_pwd)
         
     return mixed_pwd
+
+def retrieve_password():
+    with open("./accounts/accounts.json", "r") as f:
+        existing_accounts = json.load(f)
+    print("Choose one of the existing accounts:\n")
+    c = 1
+
+    for acc in existing_accounts:
+        print(f"{c} -> {acc['Platform']}")
+        c += 1
+        
+    input_choice = input("Enter the number of the account you want to retrieve: ").strip()
+    if input_choice.isdigit() and 1 <= int(input_choice) < c:
+        selected_account = existing_accounts[int(input_choice) - 1]
+        print(f"Platform: {selected_account['Platform']}")
+        print(f"Email: {selected_account['Email']}")
+        print(f"Password: {selected_account['Password']}")
+    else:
+        print("Invalid choice.")
+        
+def change_email_password():
+    with open("./accounts/accounts.json", "r") as f:
+        existing_accounts = json.load(f)
+    print("Choose one of the existing accounts:\n")
+    c = 1
+
+    for acc in existing_accounts:
+        print(f"{c} -> {acc['Platform']}")
+        c += 1
+
+    input_choice = input("Enter the number of the account you want to change: ").strip()
+    if input_choice.isdigit() and 1 <= int(input_choice) < c:
+        selected_account = existing_accounts[int(input_choice) - 1]
+        print(f"Platform: {selected_account['Platform']}")
+        print(f"Email: {selected_account['Email']}")
+        print(f"Password: {selected_account['Password']}")
+
+        print("What do you want to change?")
+        print("1 - Email")
+        print("2 - Password")
+        change_choice = input("Enter your choice (1/2): ").strip()
+
+        if change_choice == "1":
+            new_email = input("Enter the new email address: ").strip()
+            if new_email:
+                selected_account['Email'] = new_email
+        elif change_choice == "2":
+            print("Choose the action you want to do:")
+            print("1 - Generate a new password")
+            print("2 - Enter a password manually")
+            password_choice = input("Enter your choice (1/2): ").strip()
+
+            if password_choice == "1":
+                new_password = gen_pwd()
+            elif password_choice == "2":
+                new_password = input("Enter the new password: ").strip()
+                if new_password == "":
+                    print("Password cannot be empty! Please enter a valid password.")
+                    return
+            else:
+                print("Invalid choice.")
+                return
+
+            selected_account['Password'] = new_password
+
+        with open("./accounts/accounts.json", "w") as f:
+            json.dump(existing_accounts, f, indent=4)
+    else:
+        print("Invalid choice.")
 
 if __name__ == "__main__":
     main()
